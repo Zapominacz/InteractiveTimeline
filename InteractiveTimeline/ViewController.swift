2f49f19f702b327a7fa1aa9e6df0801fb80c354f
Mikołaj Styś <zapominacz@gmail.com>
2017-11-19 09:25:20 +0100

    Removed storyboard and created simple ViewController with view by code.

--- InteractiveTimeline/ViewController.swift
+++ InteractiveTimeline/ViewController.swift
@@ -9,6 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let timeline = UIView()
+    
+    override func loadView() {
+        super.loadView()
+        view.backgroundColor = .white
+        view.addSubview(timeline)
+        timeline.translatesAutoresizingMaskIntoConstraints = false
+        timeline.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        timeline.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        timeline.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        timeline.heightAnchor.constraint(equalToConstant: 200).isActive = true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
