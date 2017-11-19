23eb72887d5a73027292f731a141fe43464c0ecf
Mikołaj Styś <zapominacz@gmail.com>
2017-11-19 22:27:20 +0100

    Added scrollview for timeline

--- InteractiveTimeline/ViewController.swift
+++ InteractiveTimeline/ViewController.swift
@@ -12,16 +12,34 @@ class ViewController: UIViewController {
     
     let timeline = TimelineView()
     
+    private func setupScrollView(into view: UIView) -> UIScrollView {
+        let scrollView = UIScrollView()
+        view.addSubview(scrollView)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        scrollView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        return scrollView
+    }
+    
+    private func createTimeline(in scrollView: UIScrollView) {
+        timeline.backgroundColor = .white
+        timeline.translatesAutoresizingMaskIntoConstraints = false
+        timeline.widthAnchor.constraint(equalToConstant: 1000).isActive = true
+        timeline.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        timeline.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor).isActive = true
+        timeline.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor).isActive = true
+        timeline.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor).isActive = true
+        timeline.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor).isActive = true
+    }
+    
     override func loadView() {
         super.loadView()
         view.backgroundColor = .white
-        timeline.backgroundColor = .white
-        view.addSubview(timeline)
-        timeline.translatesAutoresizingMaskIntoConstraints = false
-        timeline.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        timeline.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        timeline.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        timeline.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        let scrollView = setupScrollView(into: view)
+        scrollView.addSubview(timeline)
+        createTimeline(in: scrollView)
     }
 
     override func viewDidLoad() {
